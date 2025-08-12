803d626af153659cc15c648418c53350cea1ba51
kyle <kkylewhite@hotmail.com>
2025-08-12 17:32:53 -0600

    template

---
+++ integer-as-sum-of-powers.py
@@ -0,0 +1,9 @@
+class Solution:
+    """
+    ex: n=10, x=2. out: 1. reas: 10 = 3^2 + 1^2
+    """
+    def numberOfWays(self, n: int, x: int) -> int:
+        return 0
+    
+if __name__ == "__main__":
+    print(0)
