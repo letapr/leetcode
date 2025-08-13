8396d642e116fed6766b4b20262cefabe6cc684a
kyle <kkylewhite@hotmail.com>
2025-08-12 19:53:56 -0600

    all cases pass

--- integer-as-sum-of-powers.py
+++ integer-as-sum-of-powers.py
@@ -1,8 +1,15 @@
 class Solution:
     """
     ex: n=10, x=2. out: 1. reas: 10 = 3^2 + 1^2
+    solution: itertate through range(n). 
+    dp[]
     """
     def numberOfWays(self, n: int, x: int) -> int:
+        self.num(n, x, [])
+        return 0
+    
+    def num(self, n:int, x: int, dp: list) -> int:
+
         return 0
     
 if __name__ == "__main__":
--- search-insert-position.py
+++ search-insert-position.py
@@ -1,6 +1,13 @@
 class Solution:
+    """
+    solution must be O(n)
+    solution: iterate, if nums[i] is >= target, return i
+    """
     def searchInsert(self, nums: list[int], target: int) -> int:
-        return 1
+        for i in range(len(nums)):
+            if nums[i] >= target:
+                return i
+        return len(nums)
     
 if __name__ == "__main__":
     print(0)
